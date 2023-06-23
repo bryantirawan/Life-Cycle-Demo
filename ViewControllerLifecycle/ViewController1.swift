aeb7b14f9425af13a084aea845492f050c111107
Bryant Irawan <94481012+bryantirawan@users.noreply.github.com>
2023-06-23 02:14:15 -0700

    initial commit

---
+++ ViewControllerLifecycle/ViewController1.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  ViewControllerLifecycle
+//
+//  Created by Angela Yu on 28/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController1: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("VC1 viewDidLoad Called")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("VC1 viewWillAppear Called")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("VC1 viewDidAppear Called")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("VC1 viewWillDisappear Called")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("VC1 viewDidDisappear Called")
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! ViewController2
+        //this will fail because while destinationVC exists bc it is just ViewController2, destinationVC.label has not been created yet so it is nil
+        //if you do destinationVC.label?.text = "Hello" then it is ok 
+        destinationVC.label?.text = "Hello"
+        
+    }
+    
+
+    
+
+}
+
